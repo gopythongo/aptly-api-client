85de39dff32d1cb6cf09a2a4676c50a5c6f2982f
Jonas Maurus <jonas-github@maurus.net>
2020-12-01 14:15:37 +0100

    make all BaseAPIClient constructur parameters available through Client

--- aptly_api/client.py
+++ aptly_api/client.py
@@ -3,6 +3,10 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from requests.auth import AuthBase
+from typing import Dict
+from typing import Union, Optional, Tuple
+
 from aptly_api.parts.misc import MiscAPISection
 from aptly_api.parts.packages import PackageAPISection
 from aptly_api.parts.publish import PublishAPISection
@@ -12,14 +16,27 @@ from aptly_api.parts.snapshots import SnapshotAPISection
 
 
 class Client:
-    def __init__(self, aptly_server_url: str, timeout: int = 60) -> None:
-        self.aptly_server_url = aptly_server_url
-        self.files = FilesAPISection(self.aptly_server_url, timeout=timeout)
-        self.misc = MiscAPISection(self.aptly_server_url, timeout=timeout)
-        self.packages = PackageAPISection(self.aptly_server_url, timeout=timeout)
-        self.publish = PublishAPISection(self.aptly_server_url, timeout=timeout)
-        self.repos = ReposAPISection(self.aptly_server_url, timeout=timeout)
-        self.snapshots = SnapshotAPISection(self.aptly_server_url, timeout=timeout)
+    def __init__(self, aptly_server_url: str, ssl_verify: Union[str, bool, None] = None,
+                 ssl_cert: Optional[Tuple[str, str]] = None, http_auth: Optional[AuthBase] = None,
+                 timeout: int = 60) -> None:
+        self.__aptly_server_url = aptly_server_url
+        args = [
+            self.__aptly_server_url,
+            ssl_verify,
+            ssl_cert,
+            http_auth,
+            timeout,
+        ]
+        self.files = FilesAPISection(*args)
+        self.misc = MiscAPISection(*args)
+        self.packages = PackageAPISection(*args)
+        self.publish = PublishAPISection(*args)
+        self.repos = ReposAPISection(*args)
+        self.snapshots = SnapshotAPISection(*args)
+
+    @property
+    def aptly_server_url(self) -> str:
+        return self.__aptly_server_url
 
     def __repr__(self) -> str:
         return "Client (Aptly API Client) <%s>" % self.aptly_server_url
