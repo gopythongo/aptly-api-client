d196511eb8114daf6b0357b6eeb61127409ab781
Greg Land <gregory.land@dedrone.com>
2024-07-25 12:54:18 -0400

    Added api/ready command
    * Added api/ready
    * Added tests for api/ready
    * Added fix for BaseApiClient.do_get. 503 status codes were getting
      turned ApltyAPIExceptions.  The new api/ready uses 503 as its status
      code when aptly is not ready.

--- aptly_api/tests/test_misc.py
+++ aptly_api/tests/test_misc.py
@@ -30,3 +30,26 @@ class MiscAPISectionTests(TestCase):
         rmock.get("http://test/api/version", text='{"droenk": "blah"}')
         with self.assertRaises(AptlyAPIException):
             self.mapi.version()
+
+    def test_ready(self, *, rmock: requests_mock.Mocker) -> None:
+        rmock.get("http://test/api/ready", text='{"Status":"Aptly is ready"}')
+        self.assertEqual(self.mapi.ready(), "Aptly is ready")
+
+    def test_ready_not_ready(self, *, rmock: requests_mock.Mocker) -> None:
+        rmock.register_uri("GET", "http://test/api/ready", status_code=503, text='{"Status":"Aptly is unavailable"}')
+        self.assertEqual(self.mapi.ready(), "Aptly is unavailable")
+
+    def test_ready_error(self, *, rmock: requests_mock.Mocker) -> None:
+        rmock.get("http://test/api/ready", text='{"droenk": "blah"}')
+        with self.assertRaises(AptlyAPIException):
+            self.mapi.ready()
+
+    def test_ready_error_bad_status_code(self, *, rmock: requests_mock.Mocker) -> None:
+        rmock.register_uri("GET", "http://test/api/ready", status_code=999, text='{"Status":"Aptly is ready"}')
+        with self.assertRaises(AptlyAPIException):
+            self.mapi.ready()
+
+    def test_ready_aptly_to_old(self, *, rmock: requests_mock.Mocker) -> None:
+        rmock.register_uri("GET", "http://test/api/ready", status_code=404, text="Not Found")
+        with self.assertRaises(NotImplementedError):
+            self.mapi.ready()
